e2034b44cc697378b9dbd3b11c3587712bb480c3
petrero <petrero@gmail.com>
2017-04-30 20:44:07 +0300

    getCredentials()
    First: getCredentials(). Our job is to read the Authorization header and return the token - if any - that's being passed. To help with this, we can use an object from the JWT bundle we installed earlier: {dollar_sign}extractor = new AuthorizationHeaderTokenExtractor(). Pass it Bearer - the prefix we're expecting before the actual token - and Authorization, the header to look on.
    
    Grab the token with {dollar_sign}token = {dollar_sign}extractor–>extract() and pass it the {dollar_sign}request.
    
    If there is no token, return null.
    
    This will cause authentication to stop. Not fail, just stop trying to authenticate the user via this method.
    
    If there is a token, return it!

--- src/AppBundle/Security/JwtTokenAuthenticator.php
+++ src/AppBundle/Security/JwtTokenAuthenticator.php
@@ -7,10 +7,21 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
 
 class JwtTokenAuthenticator extends AbstractGuardAuthenticator{
   public function getCredentials(Request $request) {
-    ;
+    $extractor = new AuthorizationHeaderTokenExtractor(
+      'Bearer',
+      'Authorization'
+    );
+    $token = $extractor->extract($request);
+    
+    if (!$token){
+      return;
+    }
+    
+    return $token;
   }
   public function getUser($credentials, UserProviderInterface $userProvider) {
     ;
