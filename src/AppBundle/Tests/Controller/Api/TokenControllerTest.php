8d8aafba706be220dcb3865f3e0dfef37e25a7f3
petrero <petrero@gmail.com>
2017-04-30 19:12:43 +0300

    Copy code from branch 4

---
+++ src/AppBundle/Tests/Controller/Api/TokenControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace AppBundle\Tests\Controller\Api;
+
+use AppBundle\Test\ApiTestCase;
+
+class TokenControllerTest extends ApiTestCase {
+  public function testPOSTCreateToken(){
+     $this->createUser('weaverryan', 'I<3Pizza');
+     
+     $response = $this->client->post('/api/tokens', [
+       'auth' => ['weaverryan', 'I<3Pizza']
+     ]);
+     $this->assertEquals(200, $response->getStatusCode());
+     $this->asserter()->assertResponsePropertyExists($response, 'token');
+  }
+}
