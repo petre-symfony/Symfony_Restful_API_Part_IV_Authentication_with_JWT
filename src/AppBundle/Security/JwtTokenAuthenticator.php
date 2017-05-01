f317d25b54be073d72c8882d721b137289bee789
petrero <petrero@gmail.com>
2017-05-01 10:07:54 +0300

    The authentication system works great! Except for how it behaves when things go wrong. When an API client tries to access a protected endpoint but forgets to send an Authorization header, they're redirected to the login page. But, why?
    Here's what's going on. Whenever an anonymous user comes into a Symfony app and tries to access a protected page, Symfony triggers something called an 'entry point'. Basically, Symfony wants to be super hip and helpful by instructing the user that they need to login. In a traditional HTML form app, that means redirecting the user to the login page.
    
    But in an api, we instruct the API client that credentials are needed by returning a 401 response. So, how can we control this entry point? In Guard authentication, you control it with the start() method.
    
    The start() Method
    
    Return a new JsonResponse and we'll just say error => 'auth required' as a start. Then, set the status code to 401.
    
    To see if it's working, copy the testRequiresAuthentication method name and run that test:
    
    ./vendor/bin/phpunit --filter testRequiresAuthentication
    Huh, it didn't change anything: we're still redirected to the login page. I thought Symfony was supposed to call our start() method in this situation? So what gives?

--- src/AppBundle/Security/JwtTokenAuthenticator.php
+++ src/AppBundle/Security/JwtTokenAuthenticator.php
@@ -7,6 +7,7 @@ use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Lexik\Bundle\JWTAuthenticationBundle\TokenExtractor\AuthorizationHeaderTokenExtractor;
 use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
 use Lexik\Bundle\JWTAuthenticationBundle\Exception\JWTDecodeFailureException;
@@ -77,6 +78,8 @@ class JwtTokenAuthenticator extends AbstractGuardAuthenticator{
   }
   
   public function start(Request $request, AuthenticationException $authException = null) {
-    ;
+    return new JsonResponse([
+      'error' => 'Authentication required'       
+    ], 401);
   }
 }
