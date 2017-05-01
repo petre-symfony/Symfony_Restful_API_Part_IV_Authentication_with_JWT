59b672bd42373cb52094ad34cbd85139d8805a57
petrero <petrero@gmail.com>
2017-05-01 08:25:06 +0300

    Hold on, that's pretty amazing! The authenticator automatically decodes the token and authenticates the user. By the time ProgrammerController is executed, our user is logged in. In fact, there's one other spot we can finally fix.
    Down on line 35, we originally had to make it look like every programmer was being created by weaverryan. Without authentication, we didn't know who was actually making the API requests, and since every Programmer needs an owner, this hack was born.
    
    Replace this with {dollar_sign}this->getUser().
    
    That's it.
    
    Our controller doesn't know or care how we were authenticated: it just cares that ->getUser() returns the correct user object.
    
    Run the test again.
    
    ./vendor/bin/phpunit --filter testPOSTProgrammerWorks src/AppBundle/Tests/Controller/Api/ProgrammerControllerTest.php
    It still passes! Welcome to our beautiful JWT authentication system. Now, time to lock down every endpoint: I don't want other users messing with my code battlers.

--- src/AppBundle/Controller/Api/ProgrammerController.php
+++ src/AppBundle/Controller/Api/ProgrammerController.php
@@ -32,7 +32,7 @@ class ProgrammerController extends BaseController {
       $this->throwApiProblemValidationException($form);
     }
 
-    $programmer->setUser($this->findUserByUsername('weaverryan'));
+    $programmer->setUser($this->getUser());
 
     $em = $this->getDoctrine()->getManager();
     $em->persist($programmer);
